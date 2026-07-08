c15e3346129ebeee7e4e4463fb80615145a440af
Radu Liviu Traian (XC-CP/ENG2-RO) <Liviu.Radu@ro.bosch.com>
2026-07-08 18:01:24 +0300

    Should build out-of-the-box

--- WebApplication1/WebApplication1.Tests/obj/Debug/net9.0/WebApplication1.Tests.AssemblyInfo.cs
+++ WebApplication1/WebApplication1.Tests/obj/Debug/net9.0/WebApplication1.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0326d748eff146f9a09d004027dcab30d0330ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
